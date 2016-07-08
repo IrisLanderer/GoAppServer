83a9a0d3fac9346ea8661ea53af87c6cc9031466
grisc <grisc@192.168.0.102>
2016-07-08 17:22:40 +0200

    doofer commit weil keine rechte...

--- GoAppServer/src/kit/edu/pse/goapp/server/servlets/GroupUserManagementServlet.java
+++ GoAppServer/src/kit/edu/pse/goapp/server/servlets/GroupUserManagementServlet.java
@@ -54,7 +54,7 @@ public class GroupUserManagementServlet extends HttpServlet {
 	 * 
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
-	 */
+	 */  
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String jsonString = request.getReader().readLine();
