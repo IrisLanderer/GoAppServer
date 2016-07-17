49305fdd64bc889e0c510942bcadc31cc369023f
TanjaMueller1 <Tanjam.herb@t-online.de>
2016-07-17 19:17:01 +0200

    Add files via upload

--- GoAppServer/src/kit/edu/pse/goapp/server/algorithm/MeetingGpsAlgorithm.java
+++ GoAppServer/src/kit/edu/pse/goapp/server/algorithm/MeetingGpsAlgorithm.java
@@ -18,97 +18,130 @@ import kit.edu.pse.goapp.server.datamodels.MeetingConfirmation;
 import kit.edu.pse.goapp.server.datamodels.Participant;
 import kit.edu.pse.goapp.server.datamodels.Tour;
 
+/**
+ * Implements Algorithm for GPS Position for Meetings.
+ */
 public class MeetingGpsAlgorithm {
 
-	public static Tour setGpsTour(Tour tour) {
-		if (!isRunning(tour)) {
-			return tour;
-		}
+    /**
+     * Sets GPS Data of participants for a Tour with various places
+     * 
+     * @param tour
+     *            Tour
+     * @return tour Tour
+     */
+    public static Tour setGpsTour(Tour tour) {
+        if (!isRunning(tour)) {
+            return tour;
+        }
 
-		List<Participant> participants = new ArrayList<>();
-		for (Participant p : tour.getParticipants()) {
-			if (p.getConfirmation() == MeetingConfirmation.CONFIRMED) {
-				participants.add(p);
-			}
-		}
+        List<Participant> participants = new ArrayList<>();
+        for (Participant p : tour.getParticipants()) {
+            if (p.getConfirmation() == MeetingConfirmation.CONFIRMED) {
+                participants.add(p);
+            }
+        }
 
-		List<Participant> biggestBlob = new ArrayList<>();
-		for (Participant p : participants) {
-			List<Participant> smallBlob = new ArrayList<>();
-			smallBlob.add(p);
-			boolean abort = false;
-			while (!abort) {
-				abort = true;
-				for (Participant inner : participants) {
-					if (!smallBlob.contains(inner)) {
-						if (isInBlob(smallBlob, inner)) {
-							smallBlob.add(inner);
-							abort = false;
-						}
-					}
-				}
-			}
-			if (smallBlob.size() > biggestBlob.size()) {
-				biggestBlob = new ArrayList<>(smallBlob);
-			}
-		}
-		List<GPS> positions = new ArrayList<>();
-		for (Participant p : biggestBlob) {
-			GpsDaoImpl dao = new GpsDaoImpl();
-			dao.setUserId(p.getUser().getId());
-			GPS gpsP = dao.userGetGPS();
-			positions.add(gpsP);
-			p.getUser().setGPS(gpsP);
-		}
+        List<Participant> biggestBlob = new ArrayList<>();
+        for (Participant p : participants) {
+            List<Participant> smallBlob = new ArrayList<>();
+            smallBlob.add(p);
+            boolean abort = false;
+            while (!abort) {
+                abort = true;
+                for (Participant inner : participants) {
+                    if (!smallBlob.contains(inner)) {
+                        if (isInBlob(smallBlob, inner)) {
+                            smallBlob.add(inner);
+                            abort = false;
+                        }
+                    }
+                }
+            }
+            if (smallBlob.size() > biggestBlob.size()) {
+                biggestBlob = new ArrayList<>(smallBlob);
+            }
+        }
+        List<GPS> positions = new ArrayList<>();
+        for (Participant p : biggestBlob) {
+            GpsDaoImpl dao = new GpsDaoImpl();
+            dao.setUserId(p.getUser().getId());
+            GPS gpsP = dao.userGetGPS();
+            positions.add(gpsP);
+            p.getUser().setGPS(gpsP);
+        }
 
-		GPS median = GPS.median(positions);
-		MeetingCenter center = new MeetingCenter(median);
-		center.setParticipants(biggestBlob);
-		return tour;
-	}
+        GPS median = GPS.median(positions);
+        MeetingCenter center = new MeetingCenter(median);
+        center.setParticipants(biggestBlob);
+        return tour;
+    }
 
-	private static boolean isInBlob(List<Participant> blob, Participant test) {
-		for (Participant p : blob) {
-			GpsDaoImpl dao = new GpsDaoImpl();
-			dao.setUserId(p.getUser().getId());
-			GPS gpsP = dao.userGetGPS();
-			dao.setUserId(test.getUser().getId());
-			GPS gpsTest = dao.userGetGPS();
-			if (GPS.isClose(gpsP, gpsTest, Tour.MAX_CENTER_DISTANCE)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Shows if a participant is in blob
+     * 
+     * @param blob
+     *            List of participants of a meeting
+     * @param test
+     *            test-participant
+     * @return boolean true if participant is in Blob, else false
+     */
+    private static boolean isInBlob(List<Participant> blob, Participant test) {
+        for (Participant p : blob) {
+            GpsDaoImpl dao = new GpsDaoImpl();
+            dao.setUserId(p.getUser().getId());
+            GPS gpsP = dao.userGetGPS();
+            dao.setUserId(test.getUser().getId());
+            GPS gpsTest = dao.userGetGPS();
+            if (GPS.isClose(gpsP, gpsTest, Tour.MAX_CENTER_DISTANCE)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public static Event setGpsEvent(Event event) {
-		if (!isRunning(event)) {
-			return event;
-		}
+    /**
+     * * Sets GPS Data of participants for an Event with a static place
+     * 
+     * 
+     * @param event
+     * @return
+     */
+    public static Event setGpsEvent(Event event) {
+        if (!isRunning(event)) {
+            return event;
+        }
 
-		for (Participant p : event.getParticipants()) {
-			if (p.getConfirmation() == MeetingConfirmation.CONFIRMED) {
-				GpsDaoImpl dao = new GpsDaoImpl();
-				dao.setUserId(p.getUser().getId());
-				GPS gps = dao.userGetGPS();
-				if (GPS.isClose(gps, event.getPlace(), Event.RADIUS)) {
-					p.getUser().setGPS(gps);
-				}
-			}
-		}
-		return event;
-	}
+        for (Participant p : event.getParticipants()) {
+            if (p.getConfirmation() == MeetingConfirmation.CONFIRMED) {
+                GpsDaoImpl dao = new GpsDaoImpl();
+                dao.setUserId(p.getUser().getId());
+                GPS gps = dao.userGetGPS();
+                if (GPS.isClose(gps, event.getPlace(), Event.RADIUS)) {
+                    p.getUser().setGPS(gps);
+                }
+            }
+        }
+        return event;
+    }
 
-	private static boolean isRunning(Meeting meeting) {
-		Timestamp stamp = new Timestamp(System.currentTimeMillis());
-		long halfHour = 30 * 60 * 1000;
-		long duration = meeting.getDuration() * 60 * 1000;
-		if ((stamp.getTime() >= meeting.getTimestamp() - halfHour)
-				&& (stamp.getTime() <= meeting.getTimestamp() + duration)) {
-			return true;
-		}
+    /**
+     * Shows if the Event is already running
+     * 
+     * @param meeting
+     *            Meeting
+     * @return boolean true if event is running, else false
+     */
+    private static boolean isRunning(Meeting meeting) {
+        Timestamp stamp = new Timestamp(System.currentTimeMillis());
+        long halfHour = 30 * 60 * 1000;
+        long duration = meeting.getDuration() * 60 * 1000;
+        if ((stamp.getTime() >= meeting.getTimestamp() - halfHour)
+                && (stamp.getTime() <= meeting.getTimestamp() + duration)) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
